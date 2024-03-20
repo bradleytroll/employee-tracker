6e34c01a1664b04662a26a9fb2c6c8ef07e59748
Bradley Troll <bradleytroll@gmail.com>
2024-03-20 17:48:15 -0400

    Adds function on start to begin inquirer questions on CLI.

--- index.js
+++ index.js
@@ -4,7 +4,46 @@ const connection = require('./db/connection');
 function start() {
     inquirer.createPromptModule([
         {
-            type: 'list'
+            type: 'list',
+            name: 'action',
+            message: 'What would you like to do?',
+            choices: [
+                'View all departments',
+                'View all roles', 
+                'View all employees', 
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an exmployee role',
+                'Exit'                
+            ]
         }
-    ])
-}
+    ]).then((answers) => {
+        switch (answers.action) {
+            case 'View all departments':
+                viewDepartments();
+                break;
+            case 'Veiw all roles':
+                viewRoles();
+                break;
+            case 'View all employees':
+                viewEmployees();
+                break;
+            case 'Add a department':
+                addDepartment();
+                break;
+            case 'Add a role':
+                addRole();
+                break;
+            case 'Add an employee':
+                addEmployee();
+                break;
+            case 'Update an employee role':
+                updateEmployeeRole();
+                break;
+            case 'Exit':
+                connection.end();
+                break;
+        }
+    })
+};
